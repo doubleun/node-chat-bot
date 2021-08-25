76ab542b34b02046071bef8b4b1e81b2c86df819
Wachirachai Nitsomboon <nitsomboon77@gmail.com>
2021-08-26 02:27:24 +0700

    Fix pleaseee

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -1,4 +1,8 @@
 require('dotenv').config();
+const axios = require('axios');
+const qs = require('qs');
+const http = require('http');
+
 const https = require('https');
 
 // Handles messages events
@@ -33,21 +37,24 @@ const callSendAPI = (sender_psid, response) => {
     "message": response
   }
 
-  https.request(
-    {
-      "uri": "https://graph.facebook.com/v2.6/me/messages",
-      "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
-      "method": "POST",
-      "json": request_body
-    }, (err, res, body) => {
-      if (!err) {
-        console.log('message sent!')
-      } else {
-        console.error("Unable to send message:" + err);
+  let access_token = qs.stringify({ access_token: process.env.PAGE_ACCESS_TOKEN })
+
+  axios
+    .post(
+      'https://reqbin.com/echo/post/json',
+      request_body,
+      { params: {
+          access_token
+        }
       }
-    }
-  );
-} 
+    )
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+  });
+}
 
 module.exports = {
   handleMessage: handleMessage,
